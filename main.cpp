b8ac1e0e884d2bf19c7fa8386dc375cb8df49bbc
zhakov <zhakov@zhakovsamsung>
2013-01-13 14:52:00 +0600

    up to NetBeans Project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: zhakov
+ *
+ * Created on 13 Январь 2013 г., 14:51
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
